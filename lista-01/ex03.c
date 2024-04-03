579bf65a4972fb9c5f0887e8a147f77b772f2353
Lucas Davi <lucasdaviribeiro46@gmail.com>
2024-04-03 09:38:00 -0300

    ex03

--- lista-01/ex03.c
+++ lista-01/ex03.c
@@ -1,7 +1,28 @@
+/*Faça um algoritmo que leia quanto você ganha por hora e o número de horas trabalhadas no
+mês. Na sequência, calcule e mostre o total do seu salário no referido mês.*/
+
 #include <stdio.h>
+
+float calculoSalario(float salarioHora, float horasTrabalhadas);
 
 int main(void)
 {
-    //teste
+    float salarioHora, salarioTotal;
+    int horasTrabalhadas;
+
+    printf("Salario por Hora: ");
+    scanf("%f", &salarioHora);
+    printf("Horas trabalhadas: ");
+    scanf("%i", &horasTrabalhadas);
+
+    salarioTotal = calculoSalario(salarioHora, horasTrabalhadas);
+
+    printf("Salario Total no mês R$%.2f\n", salarioTotal);
+
     return 0;
+}
+
+float calculoSalario(float salarioHora, float horasTrabalhadas)
+{   
+    return salarioHora * horasTrabalhadas;
 }
