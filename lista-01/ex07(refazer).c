aa3009f95bd2a133cc00704eb741bfe5f61b0853
Lucas Davi <lucasdaviribeiro46@gmail.com>
2024-04-04 09:43:21 -0300

    ex06

---
+++ lista-01/ex07(refazer).c
@@ -0,0 +1,23 @@
+/*Considere que o número de uma placa de veículo é composto por quatro algarismos. Construa
+um algoritmo que leia este número e apresente o algarismo correspondente à casa das centenas.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int placa, numeroCentenas;
+
+    printf("Entre com os 4 numeros da placa: ");
+    scanf("%i", &placa);
+
+    if(placa < 1000){
+        numeroCentenas = placa / 100;
+    }
+    else{
+        numeroCentenas = placa % 1000;
+        numeroCentenas /= 100;
+    }
+    printf("Valor dos numeros da centenas: %i\n", numeroCentenas);
+
+    return 0;
+}
