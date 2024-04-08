d2ea48e735f845b46d0c45b78ce6ef312e5e9b35
Lucas Davi <lucasdaviribeiro46@gmail.com>
2024-04-08 15:29:56 -0300

    ex11

---
+++ lista-01/ex11.c
@@ -0,0 +1,22 @@
+/*Faça um algoritmo que leia um valor de hora (hora:minutos) e informe (calcule) o total de
+minutos se passaram desde o início do dia.*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int hora, minutos, minutosTotais;
+
+    do
+    {
+        printf("Entre com as horas no modelo hh:mm ");
+        scanf("%i:%i", &hora, &minutos);
+
+    } while (hora > 24 || minutos > 60);
+    
+    minutosTotais = hora*60;
+    minutosTotais += minutos;
+
+    printf("Se passaram %i minutos desde 00:00\n", minutosTotais);
+
+}
