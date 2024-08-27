512deda37fb25e5dc73df5488879f4ba6f504409
Lucas Davi <lucasdaviribeiro46@gmail.com>
2024-08-27 10:22:16 -0300

    ex02

--- lista-02/ex02.c
+++ lista-02/ex02.c
@@ -1,9 +1,11 @@
 /*2) Faça um algoritmo que tenha como entrada 3 valores a,b,c e os imprima em ordem crescente.*/
 #include <stdio.h>
 
+void ordenaNum(int *a, int *b, int *c);
+
 int main(){
 
-    int a, b, c, aux;
+    int a, b, c;
 
     printf("Entre com o valor de de a: ");
     scanf("%i", &a);
@@ -12,23 +14,29 @@ int main(){
     printf("Entre com o valor de de c: ");
     scanf("%i", &c);
 
-    if(b < a){
-        aux = b;
-        b = a;
-        a = aux;
-    }
-    if(c < a){
-        aux = c;
-        c = a;
-        a = aux;
-    }
-    if(c < b){
-        aux = c;
-        c = b;
-        b = aux;
-    }
+    ordenaNum(&a, &b, &c);
 
     printf("%i, %i, %i\n", a, b, c);
 
     return 0;
+}
+
+void ordenaNum(int *a, int *b, int *c){
+    int aux;
+    if(*b < *a){
+        aux = *b;
+        *b = *a;
+        *a = aux;
+    }
+    if(*c < *a){
+        aux = *c;
+        *c = *a;
+        *a = aux;
+    }
+    if(*c < *b){
+        aux = *c;
+        *c = *b;
+        *b = aux;
+    }
+
 }
---
+++ lista-02/ex03.c
@@ -0,0 +1,14 @@
+/*3) Faça um algoritmo que dada a idade de um nadador classifica-o em uma das seguintes categorias:
+infantil A = 5 - 7 anos
+infantil B = 8-10 anos
+juvenil A = 11-13 anos
+juvenil B = 14-17 anos
+adulto = maiores de 18 anos*/
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+
+    return 0;
+}
